5a703a903009c7524ec7b46f70f390fb8743514c
Danil Kukhlevskiy <kukhlevskiy@gmail.com>
2012-12-07 17:15:10 +1100

    Issue #5067. Prototype JS removed

--- assets/javascripts/en_queries.js
+++ assets/javascripts/en_queries.js
@@ -1,18 +1,3 @@
-document.observe('dom:loaded', function(){
-	var end=0;
-	if ($('e_q_mine_issues'))
-		{
-		$('e_q_mine_issues').nextSiblings().each(function(e){
-			if(end==0)
-				{	
-				if((e.tagName=='A')||(e.tagName=='BR'))	
-					{
-					 $('e_q_mine_issues').insert({before: e});
-					}
-				else
-					end=1
-				}
-			});
-		}
-	
-	});
+jQuery(document).ready(function(){
+
+});
--- lib/hooks/views_sidebar_hook.rb
+++ lib/hooks/views_sidebar_hook.rb
@@ -1,8 +1,7 @@
 module RedmineEnhancedQueries
   module Hooks
     class ViewsSidebarHook < Redmine::Hook::ViewListener
-      #render_on :view_issues_sidebar_issues_bottom, :partial => 'issues/mine_issues'
-	  render_on :view_layouts_base_html_head, :partial => 'hooks/enhanced_queries/html_head'
+	    render_on :view_layouts_base_html_head, :partial => 'hooks/enhanced_queries/html_head'
     end
   end
 end
